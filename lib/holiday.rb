ecbcfb3b701da4fd78dd0e2499a20be39d6d00ad
Britny Edwards <bredwards2@gmail.com>
2018-12-12 00:57:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,5 +1,6 @@
 require 'pry'
 
+
 holiday_supplies = {
   :winter => {
     :christmas => ["Lights", "Wreath"],
@@ -133,16 +134,10 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.map do |season, holiday|
-    holiday.map do |holiday, item|
-      holiday if item.include?("BBQ")
+    holiday.map do |holiday, supplies|
+      holiday if supplies.include?("BBQ")
     end 
   end.flatten.compact
 end
 
 all_holidays_with_bbq(holiday_supplies)
-
-
-
-
-
-
